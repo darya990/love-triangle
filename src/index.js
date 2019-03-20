a691c6667717f1fdcf255133fd35305308926591
darya990 <dasha.shibkova@mail.ru>
2019-03-20 18:20:05 +0300

    feat: love triangle is done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let length = preferences.length; 
+  let triangles = 0; 
+      for (let i = 1; i<=length; i++) { 
+        j=preferences[i-1]; 
+          if (j==i) continue; 
+          k=preferences[j-1]; 
+            if(preferences[k-1]==i) { 
+            triangles++; 
+} 
+} 
+triangles=triangles/3; 
+return triangles; 
 };
